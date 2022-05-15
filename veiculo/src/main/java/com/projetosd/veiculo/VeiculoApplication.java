c822f269a743425e8a508acc470d7d626d9f17ed
pauleramdf <paulomdf741@gmail.com>
2022-05-14 22:16:14 -0300

    correção docker-compose e adição do spring actuator

--- veiculo/src/main/java/com/projetosd/veiculo/VeiculoApplication.java
+++ veiculo/src/main/java/com/projetosd/veiculo/VeiculoApplication.java
@@ -2,6 +2,7 @@ package com.projetosd.veiculo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,6 +16,13 @@ public class VeiculoApplication {
         SpringApplication.run(VeiculoApplication.class, args);
     }
 
-
-
+//
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(
+//            ServerHttpSecurity http) {
+//        return http.authorizeExchange()
+//                .pathMatchers("/actuator/**").permitAll()
+//                .anyExchange().authenticated()
+//                .and().build();
+//    }
 }
